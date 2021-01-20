b3b1b4781c385bffadca49c43a4816b6280cc655
cheap-glitch <cheap.glitch@gmail.com>
2021-01-20 18:29:49 +0100

    🚩 test: Add basic tests

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,5 +1,45 @@
-test("jest works", () => { // {{{
+import { MergingStrategy, merge } from '../src/index';
 
-	expect(true).toBe(true);
+test('merging primitives', () => { // {{{
+
+	expect(merge(null, null)).toBeNull();
+	expect(merge(2,    null)).toBeNull();
+
+	expect(merge(2,     3)).toEqual(3);
+	expect(merge(2,     '2')).toEqual('2');
+	expect(merge('foo', 'bar')).toEqual('bar');
+
+}); // }}}
+
+test('merging arrays', () => { // {{{
+
+	expect(merge(null,  [])).toEqual([]);
+	expect(merge(2,     [])).toEqual([]);
+	expect(merge('foo', [])).toEqual([]);
+
+	expect(merge([null],    [null])).toEqual([null]);
+	expect(merge(['2'],     [2])).toEqual([2]);
+	expect(merge([2],       ['2'])).toEqual(['2']);
+	expect(merge([0, 1, 2], [3])).toEqual([3]);
+	expect(merge([{}],      [])).toEqual([]);
+
+	expect(merge([null],    [null], { arrays: MergingStrategy.Concatenate })).toEqual([null, null]);
+	expect(merge(['2'],     [2],    { arrays: MergingStrategy.Concatenate })).toEqual(['2', 2]);
+	expect(merge([2],       ['2'],  { arrays: MergingStrategy.Concatenate })).toEqual([2, '2']);
+	expect(merge([0, 1, 2], [3],    { arrays: MergingStrategy.Append })).toEqual([0, 1, 2, 3]);
+	expect(merge([{}],      [],     { arrays: MergingStrategy.Append })).toEqual([{}]);
+	expect(merge([],        [{}],   { arrays: MergingStrategy.Append })).toEqual([{}]);
+
+	expect(merge([{ foo: 'bar' }], [{ bar: 'foo' }], { arrays: MergingStrategy.MergeItems })).toEqual([{ foo: 'bar', bar: 'foo' }]);
+	expect(merge([0],              [{ bar: 'foo' }], { arrays: MergingStrategy.MergeItems })).toEqual([{ bar: 'foo' }]);
+	expect(merge([{ foo: 'bar' }], [0],              { arrays: MergingStrategy.MergeItems })).toEqual([0]);
+
+}); // }}}
+
+test('merging objects', () => { // {{{
+
+	expect(merge({}, {})).toEqual({});
+
+	expect(merge({ foo: 1 }, { bar: 2 })).toEqual({ foo: 1, bar: 2 });
 
 }); // }}}
